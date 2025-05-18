dbfcbc8e82e36017760796b1f925a31a2008ec3c
QY13-gif <qiyang.24@intl.zju.edu.cn>
2025-05-18 18:45:15 +0800

    Delete task2.py

--- task2.py
+++
@@ -1,5 +0,0 @@
-def Most_frequent_amino_acid(seq, codonlist=codon_list, amino_mapping=three_letter_dic):
-    
-    sequence = Most_frequent_trinucleotide(seq)
-    amino_acid = amino_mapping.get(sequence)
-    return amino_acid
